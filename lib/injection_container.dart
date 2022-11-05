f94752c874b237ddbc8c5e60f778d02be048cee7
hanifan <hanifannurulhaq@outlook.com>
2022-11-05 14:28:10 +0700

    inject AuthBloc and it's dependency

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,8 +1,11 @@
 import 'package:get_it/get_it.dart';
+import 'package:healing_travelling/auth/bloc/auth_bloc.dart';
 import 'package:healing_travelling/core/platform/network_info.dart';
+import 'package:healing_travelling/login/data/datasource/user_local_data_source.dart';
 import 'package:healing_travelling/login/data/datasource/user_remote_data_source.dart';
 import 'package:healing_travelling/login/data/repositories/user_repository_impl.dart';
 import 'package:healing_travelling/login/domain/repositories/user_repository.dart';
+import 'package:healing_travelling/login/domain/usecase/fetch_user_data.dart';
 import 'package:healing_travelling/login/domain/usecase/post_auth.dart';
 import 'package:healing_travelling/login/presentation/bloc/login_bloc.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
@@ -13,10 +16,12 @@ final sl = GetIt.instance;
 
 Future<void> init() async {
   sl.registerFactory(() => LoginBloc(postAuth: sl()));
+  sl.registerFactory(() => AuthBloc(sl()));
 
 
   //usecase
   sl.registerLazySingleton(() => PostAuthData(sl()));
+  sl.registerLazySingleton(() => FetchUserData(sl()));
 
   //repository
   sl.registerLazySingleton<UserRepository>(
@@ -26,6 +31,9 @@ Future<void> init() async {
   //data source
   sl.registerLazySingleton<UserRemoteDataSource>(
     () => UserRemoteDataSourceImpl(client: sl())
+  );
+  sl.registerLazySingleton<UserLocalDataSource>(
+    () => UserLocalDataSourceImpl(sl())
   );
 
   //!core
