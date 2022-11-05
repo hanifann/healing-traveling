163d2da73d17b82723011b39899c6f61dec7fabd
hanifan <hanifannurulhaq@outlook.com>
2022-11-05 14:27:51 +0700

    adjust AuthBloc to clean architecture

--- lib/auth/bloc/auth_bloc.dart
+++ lib/auth/bloc/auth_bloc.dart
@@ -1,25 +1,29 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:healing_travelling/utils/shared_preference_singleton.dart';
+import 'package:healing_travelling/core/usecases/usecase.dart';
+import 'package:healing_travelling/login/domain/usecase/fetch_user_data.dart';
+
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial()) {
+  AuthBloc(this.fetch) : super(AuthInitial()) {
     on<AuthEventCheckAuth>(_onCheckAuth);
   }
+
+  final FetchUserData fetch;
+  AuthState get initislState => AuthInitial();
 
   _onCheckAuth(
     AuthEventCheckAuth event,
     Emitter<AuthState> emit,
   ) async {
-    var _token = false;
+    var localData = await fetch(NoParams());
 
-    if(_token == true){
-      emit(AuthAuthenticated());
-    } else {
-      emit(AuthUnauthenticated());
-    }
+    localData!.fold(
+      (l) => emit(AuthUnauthenticated()), 
+      (r) => emit(AuthAuthenticated())
+    );
   }
 }
